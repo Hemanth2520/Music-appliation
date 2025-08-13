14afcae1bd2600da2eeffbe42ec55fc70628b36f
Hemanth2520 <khemanth2520@gmail.com>
2025-08-13 10:19:21 +0530

    Initial commit - backend with cloudinary integration

--- server.js
+++ server.js
@@ -31,15 +31,10 @@ const corsOptions = {
 
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("CORS not allowed for this origin"));
-    }
-  },
+  origin: allowedOrigins,
   credentials: true
 }));
+
 
 // Middleware
 app.use(express.json());
